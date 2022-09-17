f218ea47844fa082b1c661d0fb279ac7620524fc
Aymar31 <43984381+Aymar31@users.noreply.github.com>
2022-09-17 15:00:18 +0200

    Update second layer head
    I propose this little update to have the second layer of the head, so the player have this real head with all layers

--- edit_skin.js
+++ edit_skin.js
@@ -3,7 +3,7 @@ const sharp = require('sharp');
 function edit_skin(Pseudo, Skin) {
     return new Promise(async (resolve, reject) => {
         await sharp(`./Skin/${Pseudo}.png`)
-            .extract({width: 32, height: 16, left: 0, top: 0})
+            .extract({width: 64, height: 16, left: 0, top: 0})
             .toFile(`./Skin/${Pseudo}-resized.png`);
         resolve(`./Skin/${Pseudo}-resized.png`)
         //paste the head on the body
